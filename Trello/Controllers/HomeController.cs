bb403d8abf94e081bc971fbb9d27329ab68fdc03
Tina <kalenenc@gmail.com>
2016-12-02 11:01:57 -0500

    initial commit

---
+++ Trello/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Trello.Models;
+
+namespace Trello.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        TrelloDbContext db = new TrelloDbContext();
+
+        public ActionResult Index()
+        {
+
+            return View(db.Cards.ToList());
+        }
+
+        [HttpPost]
+        public ActionResult PostCard(string Title)
+        {
+            Card myCard = new Card();
+            
+            myCard.Title = Title;
+
+            db.Cards.Add(myCard);
+            db.SaveChanges();
+           // return View();
+
+            return RedirectToAction("Index");
+        }
+
+        
+    }
+}
---
+++ Trello/Models/Card.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Trello.Models
+{
+    public class Card
+    {
+        public int ID { get; set; }
+        public int SortOrder { get; set; }
+        public string Title { get; set; }
+    }
+
+    public class TrelloDbContext : DbContext
+    {
+        public DbSet<Card> Cards { get; set; }
+    }
+}
